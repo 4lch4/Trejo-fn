5b3bec364dc90731345bfe694159be833d539714
Devin W. Leaman <Devin.Leaman@jbhunt.com>
2019-11-08 07:52:36 -0600

    Minor upgrade

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config')
+const { apiKey, apiToken } = require('./config')
 
 // const Trejo = require('@4lch4/trejo')
 // const trejo = new Trejo({
@@ -8,8 +8,8 @@ const config = require('./config')
 
 const Trejo = require('@4lch4/trejo')
 const trejo = new Trejo({
-  apiKey: config.apiKey,
-  apiToken: config.apiToken
+  apiKey: apiKey,
+  apiToken: apiToken
 })
 
 const getListByName = (name, lists) => {
@@ -20,54 +20,54 @@ const getListByName = (name, lists) => {
   return undefined
 }
 
+const createCard = async listId => {
+  return trejo.cards().createCard({
+    idList: listId,
+    name: 'Daily Tasks',
+    desc: 'Things I should do every day.',
+    pos: 'top'
+  })
+}
+
+const createChecklistAndItems = async cardId => {
+  const newCheckList = await trejo.checklists().createChecklist({
+    idCard: newCard.id,
+    name: 'Daily Tasks',
+    pos: 'top'
+  })
+
+  const checkItemA = await trejo.checklists().createChecklistCheckItem(newCheckList.id, {
+    name: 'Check Teams',
+    pos: '1'
+  })
+
+  const checkItemB = await trejo.checklists().createChecklistCheckItem(newCheckList.id, {
+    name: 'Check Email',
+    pos: '2'
+  })
+
+  const checkItemC = await trejo.checklists().createChecklistCheckItem(newCheckList.id, {
+    name: 'Just a test',
+    pos: '3'
+  })
+
+}
+
 const main = async () => {
   try {
-    const board = await trejo.members.getBoardByName('J. B. Hunt')
-    const lists = await trejo.boards.getBoardLists(board.id)
+    const board = await trejo.members().getBoardByName('J. B. Hunt')
+
+    console.log('board...')
+    console.log(board)
+
+    const lists = await trejo.boards().getBoardLists(board.id)
     const list = getListByName('Doing', lists)
 
-    const newCard = await trejo.cards.createCard({
-      idList: list.id,
-      name: 'Daily Tasks',
-      desc: 'Things I should do every day.',
-      pos: 'top'
-    })
+    // trejo
 
-    const newCheckList = await trejo.checklists.createChecklist({
-      idCard: newCard.id,
-      name: 'Daily Tasks',
-      pos: 'top'
-    })
 
-    const checkItemA = await trejo.checklists.createChecklistCheckItem(newCheckList.id, {
-      name: 'Check Teams',
-      pos: '1'
-    })
-
-    const checkItemB = await trejo.checklists.createChecklistCheckItem(newCheckList.id, {
-      name: 'Check Email',
-      pos: '2'
-    })
-
-    const checkItemC = await trejo.checklists.createChecklistCheckItem(newCheckList.id, {
-      name: 'Just a test',
-      pos: '3'
-    })
-
-    // const board = await trejo.members.getBoardByName('J. B. Hunt')
-    // const lists = await trejo.boards.getBoardLists(board.id)
-    // const list = getListByName(lists.data, 'Doing')
-    // console.log(`list...`)
-    // console.log(list)
-    // const newCard = await trejo.cards.createCard({
-    //   idList: list.id,
-    //   name: 'Daily Tasks',
-    //   desc: 'Things I should do every day.',
-    //   pos: 'top'
-    // })
-
-    // console.log(`newCard...`)
-    // console.log(newCard.data)
+    const newCard = await createCard(list.id)
+    const newCheckList = await createChecklistAndItems(newCard.id)
   } catch (err) { console.error(err) }
 }
 
